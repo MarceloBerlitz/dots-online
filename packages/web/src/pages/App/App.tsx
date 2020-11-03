f9403411712a1a26a65553035bcc4ef0e41a622f
Marcelo Berlitz <marceloberlitz@outlook.com>
2020-11-03 02:37:56 -0300

    Removes env variable

--- packages/web/src/pages/App/App.tsx
+++ packages/web/src/pages/App/App.tsx
@@ -10,7 +10,7 @@ import Lobby from '../Lobby';
 import Game from '../Game';
 import Over from '../Over';
 
-const socket = io(process.env.SERVER_URL || 'http://localhost:3333');
+const socket = io();
 
 let playerId: string;
 socket.on('connect', () => {
